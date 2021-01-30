d8d99bdc9c1d98985bbc287bc3b7a241339ffc6e
ahujaakash <aahuja888@gmail.com>
2021-01-30 20:58:51 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  version         = "2.2"
+  subscription_id = "12690ce1-27b2-4482-a7ec-4953a220a9cf"
+  client_id       = "5b1b8210-42d2-4bca-b66e-e67f461f1191"
+  client_secret   = "t3mEp1Cww5kDKq95_YeTR77D~hUr_4-a~w"
+  tenant_id       = "58268a60-6588-4971-a1b4-3ffe7753df00"
+  features{}
+}
